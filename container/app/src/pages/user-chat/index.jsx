eaa570e2741ecda7b49f74e8418c71d9e14592a5
digital-dahal <iamyuvraj456@gmail.com>
2023-02-01 18:08:19 +0545

    better user experince and error boundary

--- container/app/src/pages/user-chat/index.jsx
+++ container/app/src/pages/user-chat/index.jsx
@@ -193,8 +193,10 @@ const ChatBody = ({ scrollRef, chats, user, checkFileOfImage, isSending }) => {
             </button>
             <div
               className={classNames(
-                "max-w-[520px] break-words ",
+                "max-w-[120px] sm:max-w-[220px] md:max-w-[320px] break-words ",
                 message?.message.length > 60 ? "rounded-md" : "rounded-full",
+                message?.message.length > 30 && "rounded-md md:rounded-md",
+
                 checkFileOfImage(message?.message) === false && "bg-accent"
               )}
             >
