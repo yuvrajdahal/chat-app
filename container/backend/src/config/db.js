23f0d68edc403c96a3de6c92416d4402b2b7bc71
yuvrajdahal <srijald15@gmail.com>
2023-06-26 21:48:00 +0545

    Final changes

--- container/backend/src/config/db.js
+++ container/backend/src/config/db.js
@@ -4,12 +4,12 @@ import socket from "socket.io";
 const connectDb = async () => {
   let url =
     process.env.NODE_ENV === "production"
-      ? process.env.MONGO_DEV_URL
+      ? process.env.MONGO_URL
       : process.env.MONGO_DEV_URL;
   const conn = await mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  });
   console.log(`Mongoose Connected at: ${conn.connection.host}`);
 };
 
